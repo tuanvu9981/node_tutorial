425309d7f132bb7e2b4c837c08e2a24dc104daed
tuanvu0801junda <tuanvutruonglacnhatsinh0801@gmail.com>
2022-03-30 14:31:03 +0700

    findOne matching document & using await in loops

--- mongo/3-findWithCondition.js
+++ mongo/3-findWithCondition.js
@@ -10,10 +10,33 @@ async function findOneWithCondition() {
         const db = client.db(dbName);
         const personCollection = db.collection('person');
 
-        const findResult = personCollection.findOne({
+        const findResult = await personCollection.findOne({
             'fullname': 'Albert Einstein'
         })
+        // await findResult.foreach(console.dir);
+        console.log(findResult)
+
+        console.log(`\n Information about topics of ${findResult.fullname}: `);
+
+        const topicCollection = db.collection('topic_element');
+        const topicListId = findResult.topicList;
+        var topics = [];
+
+        for (const element of topicListId) {
+            const topic = await topicCollection.findOne({ '_id': element });
+            topics = [...topics, topic];
+        }
+
+        topics.forEach(element => {
+            console.log(element);
+        })
+
+
     } catch (error) {
         console.log(error);
+    } finally {
+        await client.close();
     }
 }
+
+findOneWithCondition();
